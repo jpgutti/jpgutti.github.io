e0d53d825b12d653f65cadd558f607a2a603e77d
João Pedro Guttierrez <jpgutti08@hotmail.com>
2019-05-25 21:03:52 -0300

    Updates

---
+++ precache-manifest.0c166f0d3d4ad2aa5127fbf5804d6883.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5b112f1cd5f0c0114d867ca80e4851f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16d1db9a1ac359a6217d",
+    "url": "/static/css/main.93a142c9.chunk.css"
+  },
+  {
+    "revision": "b089522e52cfe042b9cd",
+    "url": "/static/js/2.32e9a4ef.chunk.js"
+  },
+  {
+    "revision": "16d1db9a1ac359a6217d",
+    "url": "/static/js/main.0e06cefc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "63d4cdcf9e61f0d5ef845ffbca3f0165",
+    "url": "/static/media/myself.63d4cdcf.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.0c166f0d3d4ad2aa5127fbf5804d6883.js"
 );
 
 self.addEventListener('message', (event) => {
