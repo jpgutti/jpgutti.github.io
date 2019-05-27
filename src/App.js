fff864c873913d18024181dce556e32427e8c424
João Pedro Guttierrez <jpgutti08@hotmail.com>
2019-05-27 09:32:02 -0300

    typed

--- src/App.js
+++ src/App.js
@@ -1,32 +1,21 @@
 import React from 'react';
-import './App.css'
-import './components/Content'
-import Myself from './assets/myself.jpeg'
-
-import {
-  ReactText, CssText
-} from './components/Styled'
-import Content from './components/Content';
+import './App.css';
+import Typing from 'react-typing-animation';
 
 const App = () => {
   return (
-    <div id="jpgutti" className="container">
-      <div className="head d-flex justify-content-between">
-        <div className="head__left">
-          <img id="Myselfie" alt="Me" src={Myself} />
-          <div className="d-flex flex-column align-self-stretch">
-            <h1>João Pedro Lima</h1>
-            <h3>Front End Developer</h3>
-            <a href="mailto:jpgutti08@hotmail.com">jpgutti08@hotmail.com</a>
-            <span className="mt-auto">Toolbox: <ReactText>React.js</ReactText>, <CssText>CSS3</CssText></span>
-          </div>
-        </div>
-        <div className="head__right text-right">
-
-        </div>
-      </div>
-      <hr/>
-      <Content/>
+    <div id="jpgutti" className="">
+      {/* <ReactTypingEffect eraseDelay={2000} speed={50} className="Typed" text={["HELLO.", "THIS IS MY PORTFOLIO"]} /> */}
+      <Typing startDelay={1000}>
+        <span className="Typed">
+          HELLO.
+          <Typing.Backspace count={6} speed={75} delay={2000} />
+          MY NAME IS JOHN
+          <Typing.Backspace count={15} speed={75} delay={2000} />
+          AND I'LL BE YOUT GUIDE
+          <Typing.Backspace count={22} speed={75} delay={2000} />
+        </span>
+      </Typing>
     </div>
   )
 }
